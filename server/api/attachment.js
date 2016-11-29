ac7e3527de61d8e6b55acb9c122053140c386fb8
hetianqi <449165578@qq.com>
2016-11-29 16:43:32 +0800

    添加上传文件功能

---
+++ server/api/attachment.js
@@ -0,0 +1,26 @@
+/**
+ * 附件控制器
+ * @author Emmett <heron1991@163.com>
+ * @date 2016-11-28 15:56:30
+ */
+
+'use strict';
+
+var config = require('../lib/config');
+
+// 上传附件
+exports.upload = function (req, res) {
+	var attachments = [];
+	var uploadPathLen = config.path.upload.length;
+
+	req.files.forEach(function (file) {
+		attachments.push({
+			filename: file.filename,
+			filepath: '/static/upload/' + file.path.substring(uploadPathLen).replace(/\\/g, '/')
+		});
+	});
+
+	res.json({
+		attachments: attachments
+	});
+};
